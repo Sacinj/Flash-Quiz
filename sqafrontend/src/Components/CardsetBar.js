e2ef15319a4a206eadad0593af15b1b08286c0eb
Sacinj <enjae.psambueno@gmail.com>
2023-04-26 02:51:54 +0800

    HomeLayout and CardsetBar can be refreshed with firestore data will still be displayed but gubot ang pag fix sa code

--- sqafrontend/src/Components/CardsetBar.js
+++ sqafrontend/src/Components/CardsetBar.js
@@ -2,43 +2,56 @@ import { db } from "../config/firebase";
 import { getDocs, collection } from "firebase/firestore";
 import { useEffect, useState } from "react";
 import { auth } from "../config/firebase";
+import { onAuthStateChanged } from "firebase/auth";
 
 // const CardsetBar = ({userEmail}) => {
 const CardsetBar = () => {
-    const userEmail = auth?.currentUser?.email;
-    
-    //const cardSetCollectionRef = collection(db, "USERS", userEmail, "CARDSETS");
+    const loggedInEmail = auth?.currentUser?.email; //Needs checking to see if this variable is the best solution to always have userEmail value even after refreshing the page
+    const [userEmail, setUserEmail] = useState(loggedInEmail);
     const [cardSetList, setCardSetList] = useState([]);
+
 
     const getCardSets = async () => {
         try {
-            console.log("Inside getCardSets func in upper try block");
-            //const data = await getDocs(cardSetCollectionRef);
+            //console.log("Inside getCardSets func in upper try block");
             const data = await getDocs(collection(db, "USERS", userEmail, "CARDSETS"));
-            console.log(data);
+            //console.log(data);
             const filteredData = data.docs.map((doc)=>({
                 id: doc.id,
             }));
-            console.log("Inside getCardSets func in lower try block");
+            //console.log("Inside getCardSets func in lower try block");
             setCardSetList(filteredData);
-            console.log(filteredData);
+            //console.log(filteredData);
         } catch (err) {
-            console.log("Inside getCardSets func in catch block");
+            //console.log("Inside getCardSets func in catch block");
             console.error(err);
         }
     }; 
-
+    
     useEffect(() => {
-        console.log("Current user(useEffect upper, CardsetBar): ", userEmail);
+        //console.log("User that is logged in(Before authState): ",userEmail);
+        onAuthStateChanged(auth, (userData)=>{
+            if(userData){
+                //console.log(userData);
+                //console.log("THY EMAIL: ",userData.email);
+                setUserEmail(userData.email);
+                //console.log("The userEmail: ",userEmail);
+            } else{
+                console.log("Walay sulod ky wlay email");
+            }
+        });
+        //console.log("Current user(useEffect upper, CardsetBar): ", userEmail);
         if(userEmail){
             getCardSets();
+            console.log("Na get ang cardset");
+        }else{
+            console.log("Wala na get ang card set");
         }
-        
-        console.log("Current user(useEffect lower, CardsetBar): ", userEmail);
-    }, []);
+        //console.log("Current user(useEffect lower, CardsetBar): ", userEmail);
+    }, [loggedInEmail]);
 
 
-    // i cant display only qa becuase its an object, only its fields or properties
+    // i cant display only qa becuase its an object and it's the id of the row, only its fields or properties
     return(
         <section className="cardSet_bar">
                 
--- sqafrontend/src/layouts/HomeLayout.js
+++ sqafrontend/src/layouts/HomeLayout.js
@@ -1,11 +1,39 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { NavLink, Outlet, Link, useNavigate } from "react-router-dom";
 import '../styles/HomeLayout.css';
 import { auth } from "../config/firebase";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 import CardsetBar from "../Components/CardsetBar";
+import { collection } from "firebase/firestore";
 
 const HomeLayout = () => {
+    //const [userName, setUserName] = useState("");
+    const [email, setEmail] = useState("");
+    const loggedInEmail = auth?.currentUser?.email; //Needs checking to see if this variable is the best
+
+    /* const getUserName = async ( email ) => {
+        try{
+            const data = await getDocs(collection(db, "USERS"));
+            //console.log(data);
+            const filteredData = data.docs.map((doc)=>({
+                ...doc.data(),
+                id: doc.id,
+            }));
+            setScienceList(filteredData);
+        } catch (err) {
+            console.error(err);
+        }
+        
+    } */
+    useEffect(()=>{
+        onAuthStateChanged(auth, (userData)=>{
+            //console.log(userData);
+            if(userData){
+                setEmail(userData.email);
+            }
+        });
+        //getUserName();
+    },[loggedInEmail]);
     const navigate = useNavigate();
 
     const logout = async () => {
@@ -27,7 +55,19 @@ const HomeLayout = () => {
                     <article className="acct_nav">
                         <button type="button" className="logout_btn" onClick={logout} >Log Out</button>
                         <div>
+                            {/* {email} */}
                             {auth?.currentUser?.email}
+                            {/* {scienceList.map((qa)=>(
+                                if(qa.email==email){
+                                    <article className="set_box" key={qa.id}>
+                                    {qa.answer}
+                                    </article>
+                                } else{
+                                    <article className="set_box" key={qa.id}>
+                                    </article>
+                                }
+                                
+                            ))} */}
                         </div>
                         <div className="profile_pic">
                             <p>pic</p>
